ee4bcd1a670a20477461bca152184a0bc4e41fc8
Eldo Macuácua <eldomacuacua@yahoo.com>
2025-06-25 13:56:48 +0530

    add: update-3

---
+++ src/pages/bookstore/bookstore.tsx
@@ -0,0 +1,9 @@
+import BookStorePage from "../../components/sections/bookstore/BookStore";
+
+export default function Events() {
+  return (
+    <div className="space-y-28">
+      <BookStorePage />
+    </div>
+  );
+}
